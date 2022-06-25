156c094b2f89f8e76162e40b8751a746fb569e28
xhz8s <skarockoi@riseup.net>
2022-06-25 19:42:54 +0200

    pThread Readability

--- pHake/UI/pThread.hpp
+++ pHake/UI/pThread.hpp
@@ -4,77 +4,62 @@
 #include <thread>
 #include <chrono>
 
-class pThread
+class pThread // creates thread for functions that need to be run in a seperate loop
 {
+public:
+	pThread();
+
+	template <typename T>
+	pThread(T func);
+	template <typename T>
+	pThread(T func, int64_t interval); // sleep interval
+
+	void Enable();
+	void Disable();
+	void Destroy();
+	void interval(int64_t interval);
+
 private:
 	std::thread t;
 	bool    is_on_ = false;
 	bool    die_ = false;
 	int64_t interval_ = 0;
-
-public:
-	pThread() {};
-
-	template <typename T>
-	pThread(T func)
-	{
-		is_on_ = true;
-
-		t = std::thread([=]()
-			{
-				while (!die_)
-				{
-					if (is_on_)
-						func();
-
-					std::this_thread::sleep_for(std::chrono::milliseconds(interval));
-				}
-			});
-		t.detach();
-	}
-
-	template <typename T>
-	pThread(T func, int64_t interval)
-	{
-		is_on_ = true;
-		interval_ = interval;
-	
-		t = std::thread([=]()
-			{
-				while (!die_)
-				{
-					if (is_on_)
-						func();
-	
-					std::this_thread::sleep_for(std::chrono::milliseconds(interval));
-				}
-			});
-		t.detach();
-	}
-
-	~pThread()
-	{
-		t.~thread();
-	}
-
-	void Enable()
-	{
-		is_on_ = true;
-	}
-
-	void Disable()
-	{
-		is_on_ = false;
-	}
-
-	void Destroy()
-	{
-		die_ = true;
-	}
-
-	void interval(int64_t interval)
-	{
-		interval_ = interval;
-	}
 };
 #endif
+
+template<typename T>
+inline pThread::pThread(T func)
+{
+	is_on_ = true;
+
+	t = std::thread([=]()
+		{
+			while (!die_)
+			{
+				if (is_on_)
+					func();
+
+				std::this_thread::sleep_for(std::chrono::milliseconds(interval));
+			}
+		});
+	t.detach();
+}
+
+template<typename T>
+inline pThread::pThread(T func, int64_t interval)
+{
+	is_on_ = true;
+	interval_ = interval;
+
+	t = std::thread([=]()
+		{
+			while (!die_)
+			{
+				if (is_on_)
+					func();
+
+				std::this_thread::sleep_for(std::chrono::milliseconds(interval));
+			}
+		});
+	t.detach();
+}
