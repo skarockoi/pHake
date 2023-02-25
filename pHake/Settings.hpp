f46a74cadc3a0d664edea242b10b535a5118913f
xhz8s <skarockoi@riseup.net>
2023-02-25 14:07:55 +0100

    changed settings

--- pHake/Settings.hpp
+++ pHake/Settings.hpp
@@ -6,27 +6,6 @@
 #include <stdint.h>
 #include <Windows.h>
 
-struct Keys
-{
-	uint32_t menu; // VK_MENU;
-	uint32_t teleport; // VK_NUMPAD0;
-	uint32_t boost_player; // VK_NUMPAD1;
-	uint32_t boost_vehicle; // VK_NUMPAD2;
-};
-
-struct Pointers // signatures
-{
-	uintptr_t world;
-	uintptr_t waypoint;
-	uintptr_t camera_pos;
-	uintptr_t crosshair_value;
-	uintptr_t entity_aiming_at;
-	uintptr_t is_player_aiming;
-	uintptr_t asm_update_position;
-	uintptr_t asm_update_speed_z;
-	uintptr_t kmh;
-};
-
 class Settings
 {
 public:
@@ -47,8 +26,26 @@ public:
 	float noclip_speed;
 	float kmh;
 
-	Keys keys;
-	Pointers pointers;
+	struct Keys
+	{
+		uint32_t menu; // VK_MENU;
+		uint32_t teleport; // VK_NUMPAD0;
+		uint32_t boost_player; // VK_NUMPAD1;
+		uint32_t boost_vehicle; // VK_NUMPAD2;
+	}keys;
+
+	struct Pointers // signatures
+	{
+		uintptr_t world;
+		uintptr_t waypoint;
+		uintptr_t camera_pos;
+		uintptr_t crosshair_value;
+		uintptr_t entity_aiming_at;
+		uintptr_t is_player_aiming;
+		uintptr_t asm_update_position;
+		uintptr_t asm_update_speed_z;
+		uintptr_t kmh;
+	}pointers;
 
 private:
 	pINI ini;
