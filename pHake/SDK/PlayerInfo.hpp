4c2249c803ce77196785e09714ecfc69c23d5f08
xhz8s <skarockoi@riseup.net>
2023-07-05 12:30:59 +0200

    updated offsets and removed unused functions

--- pHake/SDK/PlayerInfo.hpp
+++ pHake/SDK/PlayerInfo.hpp
@@ -10,52 +10,32 @@ public:
 
 	float swim_mp()
 	{
-		return this->read<float>(0x1D0);
+		return this->read<float>(0x1C8);
 	}
 
 	void swim_mp(float value)
 	{
-		this->write<float>(0x1D0, value);
-	}
-
-	uint32_t freeze()
-	{
-		return this->read<uint32_t>(0x250);
-	}
-
-	void freeze(uint32_t value) // 2 = freeze player, 1 = freeze player and cam , 0 unfreeze
-	{
-		this->write<uint32_t>(0x250, value);
+		this->write<float>(0x1C8, value);
 	}
 
 	int32_t wanted_level()
 	{
-		return this->read<int32_t>(0x8E8);
+		return this->read<int32_t>(0x8D8);
 	}
 
 	void wanted_level(uint32_t value)
 	{
-		this->write<uint32_t>(0x8E8, value);
+		this->write<uint32_t>(0x8D8, value);
 	}
 
 	float walk_mp()
 	{
-		return this->read<float>(0xD50);
+		return this->read<float>(0xD40);
 	}
 
 	void walk_mp(float value)
 	{
-		this->write<float>(0xD50, value);
-	}
-
-	float stamina()
-	{
-		return this->read<float>(0xCF4);
-	}
-
-	void stamina(float value)
-	{
-		this->write<float>(0xCF4, value);
+		this->write<float>(0xD40, value);
 	}
 };
 #endif
